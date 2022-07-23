adfd173f0ee0db53e7c8494895d377f6cca0e4e1
Akira Funahashi <funa@bio.keio.ac.jp>
2022-07-24 06:39:04 +0900

    Add SBML validation

--- test_import.py
+++ test_import.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 import argparse
 import os
+import sys
+
+import libsbml
 import sbml2pydstool
 
 from pprint import pprint
@@ -12,7 +15,19 @@ args = parser.parse_args()
 
 if not os.path.isfile(args.sbml):
     print(args.sbml, "not found.")
-    exit(1)
+    sys.exit()
+
+# validate SBML
+print("Validating", args.sbml)
+d = libsbml.readSBML(args.sbml)
+if d.getNumErrors() > 0:
+    print(d.getErrorLog().toString())
+    sys.exit()
+
+d.setConsistencyChecks(libsbml.LIBSBML_CAT_UNITS_CONSISTENCY, False) # skip unit check
+if d.checkConsistency() > 0:
+    print(d.getErrorLog().toString())
+    sys.exit()
 
 # convert SBML to PyDSTool object
 c = sbml2pydstool.Converter(args.sbml)
